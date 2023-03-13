12b72d4691997c80d33bc80dc395ee8c5e7c4887
adaialina <114148082+adaialina@users.noreply.github.com>
2023-03-13 20:35:14 +0600

    Update app.component.ts

--- lab5/src/app/app.component.ts
+++ lab5/src/app/app.component.ts
@@ -5,7 +5,9 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent {
+  title = 'lab5';
+}
 
 /*
 Copyright Google LLC. All Rights Reserved.
