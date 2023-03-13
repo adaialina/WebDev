a1425669cf39015eb7e687fea7d6a2c9418e493b
adaialina <114148082+adaialina@users.noreply.github.com>
2023-03-13 20:29:41 +0600

    Update product-item.component.ts

--- lab5/src/app/product-item/product-item.component.ts
+++ lab5/src/app/product-item/product-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-product-item',
@@ -7,16 +7,21 @@ import { Component, Input } from '@angular/core';
 })
 export class ProductItemComponent {
   @Input() product: any
+  @Output() deleteUpdate = new EventEmitter();
 
   share(link: string) {
     document.location.href = link;
   }
 
-  onNotify() {
+  onNotify(): void {
     alert("notify");
   }
 
   formatedPrice(price: number): string {
     return price.toFixed(0).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1 ')
+  }
+
+  handleDelete(): void {
+    this.deleteUpdate.emit(this.product.id);
   }
 }
