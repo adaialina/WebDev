537c7caf56e5652bc0623a88c24e2f32f655fe83
adaialina <114148082+adaialina@users.noreply.github.com>
2023-05-16 23:01:23 +0600

    Update urls.py

--- lab8/shop_back/urls.py
+++ lab8/shop_back/urls.py
@@ -10,13 +10,19 @@ Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.urls import include, path
+    1. Import include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
 from django.urls import path
+from api.views import *
+
+from api.views import category_list, category_detail, product_list, product_detail
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls'))
+    path('api/products/', product_list),
+    path('api/products/<int:id>/', product_detail),
+    path('api/categories/', category_list),
+    path('api/categories/<int:id>/', category_detail),
 ]
